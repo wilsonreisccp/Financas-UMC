ece91833a81367de2d6930324c31e30b9bbb9ba3
Wilson Reis <wilsonreisccp@gmail.com>
2022-09-07 16:31:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+double S, RS, C1, C2, U;
+Console.WriteLine("duvida");
+Console.Write("SALAIRO : ");
+S = double.Parse(Console.ReadLine());
+
+Console.Write("CONTA ITAU : ");
+C1 = double.Parse(Console.ReadLine());
+
+Console.Write("CONTA SANTANDER : ");
+C2 = double.Parse(Console.ReadLine());
+
+RS = C1 + C1 * 2 / 100 + C2 + C2 * 2 / 100;
+
+U = S - RS;
+
+Console.WriteLine("RESTOU : " + U);
+
+Console.ReadKey();
+*/
